81560571d9de4e0699c4d75f6f09e2e34e8906a2
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-05-17 17:13:23 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,47 @@
-const CACHE_NAME = "dmezzanine-cache-v1";
+const CACHE_NAME = "cafe-system-v1";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+
+    "/",
+    "/index.html",
+    "/style.css",
+    "/script.js"
+
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener(
+'install',
+event=>{
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+        .then(cache=>{
+
+            return cache.addAll(urlsToCache);
+
+        })
+
+    );
+
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener(
+'fetch',
+event=>{
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+        .then(response=>{
+
+            return response ||
+            fetch(event.request);
+
+        })
+
+    );
+
 });
